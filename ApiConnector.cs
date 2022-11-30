8f239d5a1e1254623fa35af1f8ef4a4ca7dd154e
Niku <nokka200@gmail.com>
2022-11-30 12:14:17 +0200

    Add project files.

---
+++ ApiConnector.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System.Runtime.CompilerServices;
+
+namespace CurrencyConverter
+{
+    public class ApiConnector
+    {
+        // properties
+        public string Uri { get; set; }
+
+        // fields
+        readonly HttpClient clientObj;
+
+        public ApiConnector(string uri)
+        {
+            Uri= uri;
+            clientObj = new HttpClient();
+        }
+
+        public void TestConnection()
+        {
+            string responseBody = clientObj.GetStringAsync(Uri).Result;
+            Console.WriteLine("Body");
+            Console.WriteLine(responseBody);
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CurrencyConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ApiConnector apiObj = new("https://openexchangerates.org/api/latest.json?app_id=e4b4cecf9f2342299a29d8c2fb5786c7");
+            apiObj.TestConnection();
+        }
+    }
+}
