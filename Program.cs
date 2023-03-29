1bead5b496da4d186e8e8046036fafb5c48c43ea
Niko Niemelä <nokka200@gmail.com>
2023-03-29 10:26:55 +0300

    Aktivoitu pää metodi

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace CurrencyConverter
 
             //Console.WriteLine("-----");
 
-            //MethodTester.TestMarvelConvertToEUR();
+            MethodTester.TestMarvelConvertToEUR();
 
             //MethodTester.TestConnectionAndConvert();
 
@@ -30,7 +30,7 @@ namespace CurrencyConverter
 
             //MethodTester.TestUriObject();
 
-            MethodTester.TestDateTimeFormat();
+            //MethodTester.TestDateTimeFormat();
         }
     }
 }
